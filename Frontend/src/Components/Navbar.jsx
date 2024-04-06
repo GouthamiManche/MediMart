1c53b990492137c2b53a0369db9eb7cd5405e760
siddhantjagtap <siddhant999123@gmail.com>
2024-04-06 16:44:26 +0530

    responsive home page

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -39,8 +39,8 @@ function Navbar() {
       <header className="container mx-auto py-4 px-6 flex items-center justify-between ">
         <div className="flex items-center ">
           <img className="size-12"src="/src/Images/logo.jpg"></img>
-          <Link to="/" className="text-xl md:text-3xl font-bold transition duration-300 ease-in-out transform hover:scale-110 hover:text-blue-500">
-            MEDIMART
+          <Link to="/" className="text-xl md:text-3xl font-bold ">
+          <span className="text-blue-900">MEDI</span><span className="text-green-600">MART</span> 
           </Link>
           <nav className="hidden md:flex md:ml-[18vw]">
             <div className="flex gap-10 text-gray-900">
@@ -51,34 +51,34 @@ function Navbar() {
             </div>
           </nav>
         </div>
-        <div className="md:hidden text-gray-900">
+        <div className="md:hidden text-blue-900">
           <button onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
             {mobileMenuOpen ? <FaTimes className="text-xl" /> : <FaBars className="text-xl" />}
           </button>
         </div>
         <nav className="hidden md:flex items-center gap-4">
 
-          <Link to="/cart" className="font-bold py-2 rounded flex items-center transition duration-300 ease-in-out transform hover:scale-110 hover:text-blue-500">
-            <FaCartPlus className="text-xl" />
-            <span className="text-lg ml-1">Cart</span>
+          <Link to="/cart" className="font-bold py-2 rounded flex items-center text-blue-900">
+            <FaCartPlus className="text-xl "/>
+            <span className="text-lg ml-2">Cart</span>
           </Link>
           {showLogin ? (
-        // Render the login form
+      
         <div>
           <Link
-            className="py-2 px-4 font-bold text-lg transition duration-300 ease-in-out transform hover:scale-110 hover:text-blue-500"
+            className="py-2 px-4 font-bold text-lg text-blue-900"
             to="/login"
           >
             Login
           </Link>
         </div>
       ) : (
-        // Show the username and profile button
+    
         <div className="relative inline-block text-left">
           <div className="flex items-center">
-            <p className="py-2 px-4 font-bold text-lg">{username}</p>
+            <p className="py-2 px-4 font-bold text-lg text-blue-900">{username}</p>
             <button onClick={toggleDropdown} className="">
-          <CgProfile className="size-9 ml-2" />
+          <CgProfile className="size-9 ml-2 text-blue-900" />
           </button>
           </div>
           
@@ -86,10 +86,10 @@ function Navbar() {
             // Dropdown menu
             <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
               <div>
-                <Link to="/profile" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">
+                <Link to="/profile" className="text-blue-900 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">
                   Profile
                 </Link>
-                <Link to="/" onClick={handleLogout} type="submit" className="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">
+                <Link to="/" onClick={handleLogout} type="submit" className="text-blue-900 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">
                   Sign out
                 </Link>
               </div>
