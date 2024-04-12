29f77ce6b26ac8fe34910cefe598c10e84cf47fc
Gouthami192001 <manchegouthami@gmail.com>
2024-04-12 17:06:44 +0530

    merge

--- Backend/index.js
+++ Backend/index.js
@@ -15,8 +15,7 @@ const PORT = 4000;
 //const transporter = require('./APIS/email');
 const nodemailer = require('nodemailer');
 const URI = `mongodb+srv://mancheg19:0Pq7ouruMJz2Q9o1@cluster0.e8cib3z.mongodb.net/MediDB`
-mongoose.connect(URI, {
-});
+mongoose.connect(URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
--- Frontend/src/Components/OurProduct.jsx
+++ Frontend/src/Components/OurProduct.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import OurProductCard from './OurProductCard';
+import personalcare from '../assets/personalcare.jpg'
+import personalcare from '../assets/personalcare.jpg'
 
 export default function OurProduct() {
   return (
     <div className="m-4 md:mx-[6rem]  text-center font-semibold md:pb-[4rem]">
       <h1 className='text-xl md:text-2xl lg:mb-[2rem] lg:text-[3vw] md:pt-[2rem]'>Product Categories</h1>
       <div className="flex flex-wrap justify-center gap-4 md:gap-4  mt-8">
-        <OurProductCard imageSrc="public/personalcare.jpg" title="Personal Care" sub_category="personalcare"/>
+        <OurProductCard imageSrc={personalcare} title="Personal Care" sub_category="personalcare"/>
         <OurProductCard imageSrc="public/skincare.jpg" title="Skin Care" sub_category="skincare" />
         <OurProductCard imageSrc="public/protein_supplements.jpg" title="Protein Powders" sub_category="protein"/>
         <OurProductCard imageSrc="public/health_and_nutrition.jpg" title="Health Supplements"sub_category="supplements" />
