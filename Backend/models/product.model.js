da35a853e0afc656e51b42979350867fbbcd8774
Gouthami192001 <manchegouthami@gmail.com>
2024-05-05 21:16:35 +0530

    mess

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,23 +1,17 @@
 const mongoose = require('mongoose');
-
-const orderItemSchema = new mongoose.Schema({
-    productId: { type: String, required: true },
-    name: { type: String, required: true },
-    quantity: { type: Number, required: true },
-    price: { type: Number, required: true }
-});
-
-const orderSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    pincode: { type: String, required: true },
-    contactNo: { type: String, required: true },
-    total: { type: Number, required: true }, // Ensure total is defined as a number
-    userDetails: { type: Object },
-    // Use an array of orderItemSchema to store multiple items
-    items: [orderItemSchema]
-});
-const Order = mongoose.model('Order_Detail', orderSchema);
-module.exports = Order;
+//const ObjectID = mongoose.Schema.Types.ObjectId
+const dataSchema = new mongoose.Schema({
+    Category:String,
+    Sub_Category:String,
+    Name: String,
+    Composition: String,
+    Uses: String,
+    Side_effects: String,
+    Image_URL: String,
+    Manufacturer: String,
+    Price:Number,
+    Return_Policy:String,
+    Directions_for_Use:String
+})
+const Data = mongoose.model('product1', dataSchema);
+module.exports= Data;
