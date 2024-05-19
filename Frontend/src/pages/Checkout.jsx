ccd2113428be0a81278d08d85c3b32896170dd90
Gouthami192001 <manchegouthami@gmail.com>
2024-05-19 23:05:15 +0530

    changes

--- Frontend/src/pages/Checkout.jsx
+++ Frontend/src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState,useContext } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { SiRazorpay } from "react-icons/si";
