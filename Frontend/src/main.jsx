e99472e310d2849a8e8792d593d184f45a310b88
siddhantjagtap <siddhant999123@gmail.com>
2024-05-30 11:54:27 +0530

    Pwa

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { registerSW } from 'virtual:pwa-register';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <ToastContainer />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  registerSW();
+}
