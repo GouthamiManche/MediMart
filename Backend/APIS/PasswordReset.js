5d2b3e5801a07318724be5b3c4f12ba144cdaba8
Gouthami192001 <manchegouthami@gmail.com>
2024-06-03 15:37:37 +0530

    resetpassworddone

--- Backend/APIS/PasswordReset.js
+++ Backend/APIS/PasswordReset.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const ResetSchema = require('../models/user.model');
+const ResetPasswordSchema = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -10,30 +10,25 @@ app.use(bodyParser.json());
 
 const resetPassword = async (req, res) => {
   const { token } = req.params;
-    const { password } = req.body;
-
-    try {
-        const user = await ResetPasswordSchema.findOne({
-            resetPasswordToken: token,
-            resetPasswordExpires: { $gt: Date.now() }
-        });
-
-        if (!user) {
-            return res.status(400).json({ message: 'Password reset token is invalid or has expired.' });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        user.password = hashedPassword;
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpires = undefined;
-        await user.save();
-
-        res.json({ message: 'Password has been reset successfully.' });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: 'Internal Server Error' });
+  const { password } = req.body;
+  try {
+    const user = await ResetPasswordSchema.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpires: { $gt: Date.now() }
+    });
+    if (!user) {
+      return res.status(400).json({ message: 'Password reset token is invalid or has expired.' });
     }
-};
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+    res.json({ message: 'Password has been reset successfully.' });
+  } catch (error) {
+    console.error('Error resetting password:', error);
+    res.status(500).send({ message: 'An error occurred while resetting the password.' });
+  }
+}
 
-module.exports ={resetPassword};
+module.exports = { resetPassword };
