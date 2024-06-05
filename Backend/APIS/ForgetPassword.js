f25c1edb4c240475dc1f8795e02f9817da44d35a
Gouthami192001 <manchegouthami@gmail.com>
2024-06-05 14:17:29 +0530

    forgetpassword

--- Backend/APIS/ForgetPassword.js
+++ Backend/APIS/ForgetPassword.js
@@ -37,7 +37,7 @@ const forgotPassword = async (req, res) => {
             subject: 'Reset your password',
             text: `You are receiving this because you (or someone else) have requested to reset the password for your account.\n\n` +
                 `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-                `${FRONTEND_URL}/reset-password/${token}\n\n` +
+                `${FRONTEND_URL}reset-password/${token}\n\n` +
                 `If you did not request this, please ignore this email and your password will remain unchanged.\n`
         };
 
